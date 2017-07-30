9614cdc2f775b26184ca85fdfdd0b8c009ee30be
Nihar Rao <raonihar1997@gmail.com>
2017-07-30 20:44:44 +0530

    common python 3rd party modules.
    get started with some great 3rd party python modules

---
+++ python_quickstart.sh
@@ -0,0 +1,15 @@
+#Author- Nihar Rao
+sudo apt-get -y install python3-pip
+pip install send2trash
+pip install requests
+pip install beautifulsoup4
+pip install selenium
+pip install openpyxl==2.1.4
+pip install PyPDF2
+pip install python-docx 
+pip install imapclient
+pip install pyzmail
+pip install twilio
+pip install pillow
+pip install python3-xlib 
+pip install pyautogui
